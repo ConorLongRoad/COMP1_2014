5435ac1be10e955bbaa8cdf72b0b9abfa3af8669
Conor Woollatt <30541@longroad.ac.uk>
2014-06-30 10:01:33 +0100

    Task 2c - Connecting the graphics to the crop classes
    In this one, I was able to connect the graphics part of the coding to
    the potato and wheat classes, so that I was able to display the data
    from within them.

--- Graphical Simulator/Crop_Simulator.py
+++ Graphical Simulator/Crop_Simulator.py
@@ -4,6 +4,8 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
 from Radio_Button_Widget_Class import * #Provides the radio button widget
+from Wheat_Class import *
+from Potato_Class import *
 
 class crop_window(QMainWindow):
     """This class creates a main window to observe the growth of a simulated crop"""
@@ -29,6 +31,18 @@ class crop_window(QMainWindow):
         self.selectCropWidget.setLayout(self.initialLayout)
 
         self.setCentralWidget(self.selectCropWidget)
+
+        #Connections
+        self.instantiateButton.clicked.connect(self.instantiateCrop)
+
+    def instantiateCrop(self):
+        cropType = self.cropRadioButtons.selectedButton() #Get the radio that was clicked
+
+        if cropType == 1:
+            self.simulatedCrop = Wheat()
+        elif cropType == 2:
+            self.simulatedCrop = Potato()
+        print(self.simulatedCrop)
         
 def main():
     cropSimulation = QApplication(sys.argv) #Creatre new application
